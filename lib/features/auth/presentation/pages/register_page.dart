5cda2d533a5d275113838cc3851818aeb62b872c
bayan_ar <bayanar00@gmail.com>
2025-11-08 02:17:20 +0300

    update

--- lib/features/auth/presentation/pages/register_page.dart
+++ lib/features/auth/presentation/pages/register_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../widgets/auth_layout.dart';
+import '../widgets/register_form.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AuthLayout(showBackButton: true, child: RegisterForm());
+    return AuthLayout(
+      showBackButton: true,
+      title: 'sign_up'.tr,
+      subtitle: 'sign_up_to_create_account'.tr,
+      child: RegisterForm(),
+    );
   }
 }
